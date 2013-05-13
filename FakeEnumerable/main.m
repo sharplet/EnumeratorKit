740dfbd073338f5cd046a1b8d5e5cb77a3163c04
Adam Sharp <adam.sharp@outware.com.au>
2013-05-13 16:54:38 +1000

    Initial Commit

---
+++ FakeEnumerable/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FakeEnumerable
+//
+//  Created by Adam Sharp on 13/05/13.
+//  Copyright (c) 2013 Adam Sharp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ FakeEnumerableTests/FakeEnumerableTests.m
@@ -0,0 +1,32 @@
+//
+//  FakeEnumerableTests.m
+//  FakeEnumerableTests
+//
+//  Created by Adam Sharp on 13/05/13.
+//  Copyright (c) 2013 Adam Sharp. All rights reserved.
+//
+
+#import "FakeEnumerableTests.h"
+
+@implementation FakeEnumerableTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in FakeEnumerableTests");
+}
+
+@end
