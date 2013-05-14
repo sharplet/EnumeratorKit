fae0db46449d2ebced4eef62273b098f9996402b
Adam Sharp <adam.sharp@outware.com.au>
2013-05-14 17:46:52 +1000

    Extract SuppressPerformSelectorMemoryWarnings into shared header

--- FakeEnumerable/FakeEnumerable.m
+++ FakeEnumerable/FakeEnumerable.m
@@ -7,14 +7,7 @@
 //
 
 #import "FakeEnumerable.h"
-
-#define SuppressPerformSelectorLeakWarning(Stuff) \
-            do { \
-                _Pragma("clang diagnostic push") \
-                _Pragma("clang diagnostic ignored \"-Warc-performSelector-leaks\"") \
-                    Stuff; \
-                _Pragma("clang diagnostic pop") \
-            } while (0)
+#import "SuppressPerformSelectorMemoryWarnings.h"
 
 @implementation FakeEnumerable
 
