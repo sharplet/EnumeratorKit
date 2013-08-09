7a6f9bd873ea97f9a5e129e5ef6cc12ea216f9cb
Adam Sharp <adam.sharp@outware.com.au>
2013-08-09 18:06:50 +1000

    Improve [NSArray flatten] doc formatting

--- EnumeratorKit/Core/NSArray+EKFlatten.h
+++ EnumeratorKit/Core/NSArray+EKFlatten.h
@@ -10,20 +10,20 @@
 #import "NSArray+EKEnumerable.h"
 
 /**
- * Extends NSArray with a single method `-flatten` to recursively
- * build a single-level array with all objects from all child arrays.
+ Extends NSArray with a single method `-flatten` to recursively build a
+ single-level array with all objects from all child arrays.
  */
 @interface NSArray (EKFlatten)
 
 /**
- * Recursively builds a new array by adding all objects from all child
- * arrays. The order of children is preserved. For example:
- *
- *     [@[@[@1, @2], @3] flatten] // => @[@1, @2, @3]
- *
- * and
- *
- *     @[@1, @[@2, @[@3, @4]], @5, @[@6]] // => @[@1, @2, @3, @4, @5, @6]
+ Recursively builds a new array by adding all objects from all child
+ arrays. The order of children is preserved. For example:
+
+    [@[@[@1, @2], @3] flatten] // => @[@1, @2, @3]
+
+ and
+
+    @[@1, @[@2, @[@3, @4]], @5, @[@6]] // => @[@1, @2, @3, @4, @5, @6]
  */
 - (NSArray *)flatten;
 
