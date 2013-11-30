2eba264412592aea964531468c6245bbc7e8a448
Adam Sharp <adam.sharp@outware.com.au>
2013-11-30 11:39:30 +1100

    Deprecate NSDictionary block property methods

--- EnumeratorKit/Core/NSDictionary+EKEnumerable.h
+++ EnumeratorKit/Core/NSDictionary+EKEnumerable.h
@@ -13,13 +13,13 @@
 
 - (instancetype)eachEntry:(void (^)(id entry))block;
 - (instancetype)eachPair:(void (^)(id pair))block;
-- (id<EKEnumerable> (^)(void (^)(id entry)))eachEntry;
-- (id<EKEnumerable> (^)(void (^)(id pair)))eachPair;
+- (id<EKEnumerable> (^)(void (^)(id entry)))eachEntry DEPRECATED_ATTRIBUTE;
+- (id<EKEnumerable> (^)(void (^)(id pair)))eachPair DEPRECATED_ATTRIBUTE;
 
 - (instancetype)eachKey:(void (^)(id key))block;
-- (id<EKEnumerable> (^)(void (^)(id key)))eachKey;
+- (id<EKEnumerable> (^)(void (^)(id key)))eachKey DEPRECATED_ATTRIBUTE;
 
 - (instancetype)eachObject:(void (^)(id obj))block;
-- (id<EKEnumerable> (^)(void (^)(id obj)))eachObject;
+- (id<EKEnumerable> (^)(void (^)(id obj)))eachObject DEPRECATED_ATTRIBUTE;
 
 @end
