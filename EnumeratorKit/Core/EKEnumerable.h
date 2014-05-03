5151715325678e006ebd8098d5cbdaea38d428fb
Adam Sharp <adsharp@me.com>
2014-05-03 16:47:43 +1000

    Minor documentation reformatting

--- EnumeratorKit/Core/EKEnumerable.h
+++ EnumeratorKit/Core/EKEnumerable.h
@@ -444,19 +444,19 @@
 
  Usage:
 
- NSArray *numbers = @[@1, @3, @5, @7, @9];
+     NSArray *numbers = @[@1, @3, @5, @7, @9];
 
- // look for an even number
- [numbers any:^BOOL(id obj) {
- return [obj integerValue] % 2 == 0;
- }];
- // => @NO
+    // look for an even number
+    [numbers any:^BOOL(id obj) {
+        return [obj integerValue] % 2 == 0;
+    }];
+    // => @NO
 
  @param block A block that accepts each successive object and returns
- a `BOOL` result.
+    a `BOOL` result.
 
- @return Returns `YES` if an object is found for which the block returns `YES`
- If no object is found, returns `NO`.
+ @return Returns `YES` if an object is found for which the block returns `YES`.
+    If no object is found, returns `NO`.
  */
 - (BOOL)any:(BOOL (^)(id obj))block;
 
@@ -465,19 +465,19 @@
 
  Usage:
 
- NSArray *numbers = @[@1, @3, @5, @7, @9];
+     NSArray *numbers = @[@1, @3, @5, @7, @9];
 
- // Check if all numbers are odd
- [numbers all:^BOOL(id obj) {
- return [obj integerValue] % 2 != 0;
- }];
- // => @YES
+    // Check if all numbers are odd
+    [numbers all:^BOOL(id obj) {
+        return [obj integerValue] % 2 != 0;
+    }];
+    // => @YES
 
  @param block A block that accepts each successive object and returns
- a `BOOL` result.
+    a `BOOL` result.
 
- @return Returns `YES` if the block returns `YES` for all objects in the collection.
- If the block returns `NO` for at least one object, returns `NO`.
+ @return Returns `YES` if the block returns `YES` for all objects in the
+    collection. If the block returns `NO` for at least one object, returns `NO`.
  */
 - (BOOL)all:(BOOL (^)(id obj))block;
 
